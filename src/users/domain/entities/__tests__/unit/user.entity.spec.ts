a9a896af4352e15a4469479591654780729cbc56
Kleyton João <kleyton.joao@gmail.com>
2024-04-01 13:10:34 -0300

    UserEntity - getters methods unit tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -6,21 +6,45 @@ describe('UserEntity unit tests', () => {
   let props: UserProps
   let sut: UserEntity
 
-  beforeEach(()=> {
+  beforeEach(() => {
     props = {
       name: faker.person.fullName(),
       email: faker.internet.email(),
       password: faker.internet.password()
-     }
-      sut = new UserEntity(props)
+    }
+    sut = new UserEntity(props)
   })
 
   it('Constructor method', () => {
 
-     expect(sut.props.name).toEqual(props.name)
-     expect(sut.props.email).toEqual(props.email)
-     expect(sut.props.password).toEqual(props.password)
-     expect(sut.props.createdAt).toBeInstanceOf(Date)
+    expect(sut.props.name).toEqual(props.name)
+    expect(sut.props.email).toEqual(props.email)
+    expect(sut.props.password).toEqual(props.password)
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+
+  })
+
+  it('Get of name field', () => {
+    expect(sut.props.name).toBeDefined()
+    expect(sut.props.name).toEqual(props.name)
+    expect(typeof sut.props.name).toBe('string')
+  })
+
+  it('Get of email field', () => {
+    expect(sut.props.email).toBeDefined()
+    expect(sut.props.email).toEqual(props.email)
+    expect(typeof sut.props.email).toBe('string')
+  })
+
+  it('Get of password field', () => {
+    expect(sut.props.password).toBeDefined()
+    expect(sut.props.password).toEqual(props.password)
+    expect(typeof sut.props.password).toBe('string')
+  })
+
+  it('Get of password createdAt', () => {
+    expect(sut.props.createdAt).toBeDefined()
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
 
   })
 
