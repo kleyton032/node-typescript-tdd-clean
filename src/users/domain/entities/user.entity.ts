68039611867c2c9dd0dd9906991887f3ef454f17
Kleyton João <kleyton.joao@gmail.com>
2024-04-01 16:12:41 -0300

    adding UserDatabuilder helper method

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -2,7 +2,7 @@ export type UserProps = {
   name: string,
   email: string,
   password: string,
-  createdAt?: Date
+  createdAt?: Date | string
 
 }
 
@@ -11,6 +11,21 @@ export class UserEntity {
   constructor(public readonly props: UserProps) {
     this.props.createdAt = this.props.createdAt ?? new Date()
   }
+
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+  get password() {
+    return this.props.password
+  }
+  get createdAt() {
+    return this.props.createdAt
+  }
+
 
 
 
