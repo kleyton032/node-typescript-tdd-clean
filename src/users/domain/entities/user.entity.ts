3d8d400af1762dda0270f616bbddab86f217aaf4
Kleyton João <kleyton.joao@gmail.com>
2024-04-01 12:31:06 -0300

    UserEntity - constructor unit tests

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,3 +1,17 @@
+export type UserProps = {
+  name: string,
+  email: string,
+  password: string,
+  createdAt?: Date
+
+}
+
 export class UserEntity {
+
+  constructor(public readonly props: UserProps) {
+    this.props.createdAt = this.props.createdAt ?? new Date()
+  }
+
+
 
 }
