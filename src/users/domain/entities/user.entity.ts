228911a5d17c9ca1fe9441362fd18c3094711762
Kleyton João <kleyton.joao@gmail.com>
2024-04-02 09:13:39 -0300

    adding abstract class entity using with uuid get id

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,3 +1,5 @@
+import { Entity } from "@/shared/domain/entities/entity"
+
 export type UserProps = {
   name: string,
   email: string,
@@ -6,9 +8,10 @@ export type UserProps = {
 
 }
 
-export class UserEntity {
+export class UserEntity extends Entity<UserProps> {
 
-  constructor(public readonly props: UserProps) {
+  constructor(public readonly props: UserProps, id?: string) {
+    super(props, id)
     this.props.createdAt = this.props.createdAt ?? new Date()
   }
 
