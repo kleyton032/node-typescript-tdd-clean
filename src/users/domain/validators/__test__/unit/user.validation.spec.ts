830d419d54d7fbf7b3129c2c873170f533e02a27
Kleyton João <kleyton.joao@gmail.com>
2024-04-09 16:33:24 -0300

    implementation unit tests UserValidation

--- src/users/domain/validators/__test__/unit/user.validation.spec.ts
+++ src/users/domain/validators/__test__/unit/user.validation.spec.ts
@@ -1,14 +1,18 @@
+import { UserDataBuilder } from "@/users/domain/testing/helpers/user-data-builder"
 import { UserValidator, UserValidatorFactory } from "../../user.validator"
+import { UserProps } from "@/users/domain/entities/user.entity"
 
 let sut: UserValidator
+let props: UserProps
 
 describe('UserValidator unit tests', () => {
   beforeEach(() => {
     sut = UserValidatorFactory.create()
+    props = UserDataBuilder({})
   })
   describe('Name field', () => {
     it("Invalidation cases for name field", () => {
-      const isValid = sut.validate(null as any)
+      let isValid = sut.validate(null as any)
       expect(isValid).toBeFalsy()
       expect(sut.errors['name']).toStrictEqual(
         [
@@ -16,6 +20,23 @@ describe('UserValidator unit tests', () => {
           'name must be a string',
           'name must be shorter than or equal to 255 characters'
         ])
+
+      isValid = sut.validate({ ...props, name: '' })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['name']).toStrictEqual(['name should not be empty'])
+
+      isValid = sut.validate({ ...props, name: 10 as any })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['name']).toStrictEqual([
+        'name must be a string',
+        'name must be shorter than or equal to 255 characters',
+      ])
+
+      isValid = sut.validate({ ...props, name: 'a'.repeat(256) })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['name']).toStrictEqual([
+        'name must be shorter than or equal to 255 characters',
+      ])
     })
   })
 })
