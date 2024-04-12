64ef90bf6e73553c2d1954fceee2dc077a564a8b
Kleyton João <kleyton.joao@gmail.com>
2024-04-12 14:26:56 -0300

    continued unit tests for userValidator

--- src/users/domain/validators/__test__/unit/user.validation.spec.ts
+++ src/users/domain/validators/__test__/unit/user.validation.spec.ts
@@ -1,5 +1,5 @@
 import { UserDataBuilder } from "@/users/domain/testing/helpers/user-data-builder"
-import { UserValidator, UserValidatorFactory } from "../../user.validator"
+import { UserRules, UserValidator, UserValidatorFactory } from "../../user.validator"
 import { UserProps } from "@/users/domain/entities/user.entity"
 
 let sut: UserValidator
@@ -10,8 +10,7 @@ describe('UserValidator unit tests', () => {
     sut = UserValidatorFactory.create()
     props = UserDataBuilder({})
   })
-  describe('Name field', () => {
-    it("Invalidation cases for name field", () => {
+  it("Invalidation cases for name field", () => {
       let isValid = sut.validate(null as any)
       expect(isValid).toBeFalsy()
       expect(sut.errors['name']).toStrictEqual(
@@ -37,6 +36,92 @@ describe('UserValidator unit tests', () => {
       expect(sut.errors['name']).toStrictEqual([
         'name must be shorter than or equal to 255 characters',
       ])
-    })
   })
-})
+
+  it('Invalidation cases for email field', () => {
+    let isValid = sut.validate(null as any)
+    expect(isValid).toBeFalsy()
+    expect(sut.errors['email']).toStrictEqual([
+      'email should not be empty',
+      'email must be an email',
+      'email must be a string',
+      'email must be shorter than or equal to 255 characters',
+    ])
+
+    isValid = sut.validate({ ...props, email: '' })
+    expect(isValid).toBeFalsy()
+    expect(sut.errors['email']).toStrictEqual([
+      'email should not be empty',
+      'email must be an email',
+    ])
+
+    isValid = sut.validate({ ...props, email: 10 as any })
+    expect(isValid).toBeFalsy()
+    expect(sut.errors['email']).toStrictEqual([
+      'email must be an email',
+      'email must be a string',
+      'email must be shorter than or equal to 255 characters',
+    ])
+
+    isValid = sut.validate({ ...props, email: 'a'.repeat(256) })
+    expect(isValid).toBeFalsy()
+    expect(sut.errors['email']).toStrictEqual([
+      'email must be an email',
+      'email must be shorter than or equal to 255 characters',
+    ])
+  })
+
+  it('Invalidation cases for password field', () => {
+    let isValid = sut.validate(null as any)
+    expect(isValid).toBeFalsy()
+    expect(sut.errors['password']).toStrictEqual([
+      'password should not be empty',
+      'password must be a string',
+      'password must be shorter than or equal to 100 characters',
+    ])
+
+    isValid = sut.validate({ ...props, password: '' })
+    expect(isValid).toBeFalsy()
+    expect(sut.errors['password']).toStrictEqual([
+      'password should not be empty',
+    ])
+
+    isValid = sut.validate({ ...props, password: 10 as any })
+    expect(isValid).toBeFalsy()
+    expect(sut.errors['password']).toStrictEqual([
+      'password must be a string',
+      'password must be shorter than or equal to 100 characters',
+    ])
+
+    isValid = sut.validate({ ...props, password: 'a'.repeat(256) })
+    expect(isValid).toBeFalsy()
+    expect(sut.errors['password']).toStrictEqual([
+      'password must be shorter than or equal to 100 characters',
+    ])
+  })
+
+  it('Invalidation cases for createdAt field', () => {
+    let isValid = sut.validate({ ...props, createdAt: 10 as any })
+    expect(isValid).toBeFalsy()
+    expect(sut.errors['createdAt']).toStrictEqual([
+      'createdAt must be a Date instance',
+    ])
+
+    isValid = sut.validate({ ...props, createdAt: '2023' as any })
+    expect(isValid).toBeFalsy()
+    expect(sut.errors['createdAt']).toStrictEqual([
+      'createdAt must be a Date instance',
+    ])
+  })
+
+  it("valid cases for name field", () => {
+    const props = UserDataBuilder({})
+    const isValid = sut.validate(props)
+    expect(isValid).toBeTruthy()
+    expect(sut.validatedData).toStrictEqual(new UserRules(props))
+  })
+
+  })
+
+
+
