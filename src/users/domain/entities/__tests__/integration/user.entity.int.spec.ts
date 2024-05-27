9f5b4069642565f6191a4e8e0b570c922d4f6b43
Kleyton João <kleyton.joao@gmail.com>
2024-05-27 14:49:54 -0300

    Integration tests - Update method

--- src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -116,4 +116,24 @@ describe('UserEntity integration tests', () => {
       new UserEntity(props)
     })
   })
+
+  describe('Update Method', () => {
+    it('Should throw an error when update a user with invalid name', () => {
+      const entity = new UserEntity(UserDataBuilder({}))
+      expect(() => entity.update(null)).toThrow(EntityValidationError)
+      expect(() => entity.update('')).toThrow(EntityValidationError)
+      expect(() => entity.update(10 as any)).toThrow(EntityValidationError)
+      expect(() => entity.update('a'.repeat(256))).toThrow(EntityValidationError)
+    })
+
+    it('Should a valid name', () => {
+      expect.assertions(0)
+
+      const props: UserProps = {
+        ...UserDataBuilder({})
+      }
+      const entity = new UserEntity(props)
+      entity.update('other name')
+    })
+  })
 })
